aa515651aa8087df27d16c668c96166ef52780ac
olivewilles-rgb <olivewilles@gmail.com>
2026-06-03 08:45:36 -0700

    Create two.java

---
+++ two.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class two {
+    public static void go() {
+        Scanner input = new Scanner(System.in);
+        x.clearScreen();
+        x.pl(Colors.BLUE + "You have arrived at school");
+        x.simpleEnter();
+        x.pl("Here's your schedule: ");
+        x.simpleEnter();
+        x.pl("Classes:");
+        x.pl("8:00 - 8:57: Dissection");
+        x.pl("12:00 - 2:34: Inspection");
+        x.pl("3:15 - 3:40: Writing");
+        x.simpleEnter();
+        x.pl("Do you like your schedule?");
+        x.pl("Yes or no");
+        String answer = input.nextLine();
+        
+        if(answer.equals("Yes") || answer.equals("yes")){
+            x.pl("Good because you can't change it.");
+        }
+        else{
+            x.pl("Doesn't matter because you can't change it.");
+        }
+        
+        x.simpleEnter();
+        x.p("OK.");
+        x.noLineEnter();
+        x.p(" Sorry.");
+        x.simpleEnter();
+        x.p(" School is going to be really fun.");
+        x.simpleEnter();
+        x.pl(" Please get to class and stop wasting time.");
+        
+        x.simpleEnter();
+        three.go();
+    }
+}
