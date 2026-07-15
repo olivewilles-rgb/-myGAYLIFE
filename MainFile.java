55de69e8ea268cb2cde572e24cad2a782c2feb5a
olivewilles-rgb <olivewilles@gmail.com>
2026-07-15 11:51:06 -0700

    Update MainFile.java

--- MainFile.java
+++ MainFile.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 public class MainFile{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println(Colors.RED + "#myGAYLIFE by Olive Willes");
+        System.out.print(Colors.RED + "#myGAYLIFE by Olive Willes");
         x.l();
         TheImages images = new TheImages();
         String flagArt = images.getNotSure("Flag");
         if (flagArt != null) {
-            System.out.println(flagArt);
+            System.out.print(flagArt);
         } else {
             System.out.println("[Flag image missing]");
         }
