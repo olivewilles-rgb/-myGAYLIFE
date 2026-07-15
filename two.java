fd213667caaf17e8643e09c64d9855f8231a0882
olivewilles-rgb <olivewilles@gmail.com>
2026-07-15 12:28:48 -0700

    Refactor schedule output with image handling
    Updated schedule display to use images and improved text formatting.

--- two.java
+++ two.java
@@ -7,13 +7,18 @@ public class two {
         x.simpleEnter();
         x.pl("Here's your schedule: ");
         x.simpleEnter();
-        x.pl("Classes:");
-        x.pl("8:00 - 8:57: Inspection");
-        x.pl("12:00 - 2:34: Dissection");
-        x.pl("3:15 - 3:40: Writing");
+        TheImages images = new TheImages();
+        String cA = images.getNotSure("Classes");
+        if (cA != null) {
+            System.out.println(cA);
+        } else {
+            System.out.println("[Class image missing]");
+        }
         x.simpleEnter();
         x.pl("Do you like your schedule?");
-        x.pl("Yes or no");
+        x.pl("Yes or No");
+        x.pl("");
+        x.p("Type your answer: ");
         String answer = input.nextLine();
         
         if(answer.equals("Yes") || answer.equals("yes")){
@@ -35,7 +40,7 @@ public class two {
         x.p("School is going to be really fun.");
         x.pl("");
         x.simpleEnter();
-        x.p("🤑😩😩😩🤏😁");
+        x.p("🤑 😩 😩 😩 🤏 😁");
         x.pl("");
         x.simpleEnter();
         x.pl("Please get to class and stop wasting time.");
