5cbeb01c33f259eccd580ef911633a22221d2f58
olivewilles-rgb <olivewilles@gmail.com>
2026-07-22 13:24:26 -0700

    Update Colors.java

--- Colors.java
+++ Colors.java
@@ -1,6 +1,7 @@
 public class Colors {
     public static final String BLACK = "\u001B[30m";
     public static final String RED = "\u001B[31m";
+    public static final String ORANGE = "\u001B[38;5;208m";
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
     public static final String BLUE = "\u001B[34m";
